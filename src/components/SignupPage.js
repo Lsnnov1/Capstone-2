1fe6b56416801480db8e6fe1d1fb2f2f8220942e
Lsnnov1 <154484671+Lsnnov1@users.noreply.github.com>
2024-12-24 11:45:45 -0500

    signuppage

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const SignUpPage = () => {
+const SignupPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
